7150036bcd8dd1dd6de7d70d550ed441c3c70ae4
harveytriana <harveytriana@hotmail.com>
2021-10-22 11:11:51 -0500

    Simply CPP

--- BlazorCrank/CPP/Operations.cpp
+++ BlazorCrank/CPP/Operations.cpp
@@ -1,5 +1,5 @@
 /*
-	Operations.cpp
+	CPP/Operations.cpp
 */
 #ifdef _WIN32
 #  define E __declspec( dllexport )
@@ -19,17 +19,9 @@ struct  Legs
 using namespace std;
 
 extern "C" {
-	E int add(int a, int b) {
-		return a + b;
-	}
-
-	E int subtract(int a, int b) {
-		return a - b;
-	}
-
 	E float hypotenuse(Legs legs) {
-		cout << "X: " << legs.X << endl;
-		cout << "Y: " << legs.Y << endl;
+		cout << "LEG X: " << legs.X << endl;
+		cout << "LEG Y: " << legs.Y << endl;
 		return sqrt(pow(legs.X, 2.0) + pow(legs.Y, 2.0));
 	}
 }
