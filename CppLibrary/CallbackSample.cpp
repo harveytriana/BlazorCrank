1118aff4f82457401c34f407aa736dd31c9f0d99
harveytriana <harveytriana@hotmail.com>
2021-10-23 20:58:24 -0500

    Sleep in C++

--- CppLibrary/CallbackSample.cpp
+++ CppLibrary/CallbackSample.cpp
@@ -1,5 +1,9 @@
 #define E __declspec(dllexport)
 #include <iostream>
+#include <chrono>
+#include <thread>
+
+using namespace std;
 
 // delegate
 typedef void(__stdcall* PFN_PROMPT)(int number);
@@ -7,7 +11,12 @@ typedef void(__stdcall* PFN_PROMPT)(int number);
 // implementation
 extern "C" {
 	E void __stdcall UnmanagedPrompt(PFN_PROMPT fn) {
-		// something...
-		fn(1234);
+
+		for (int i = 1; i <= 10; i++)
+		{
+			// by example
+			this_thread::sleep_for(chrono::milliseconds(1000));
+			fn(i);
+		}
 	};
 }
