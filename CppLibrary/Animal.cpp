74219ff48c8db6a245b7477177f0d1fdbda300f6
AlanAssembly <harveytriana@hotmail.com>
2021-12-28 15:31:39 -0500

    "C"

--- CppLibrary/Animal.cpp
+++ CppLibrary/Animal.cpp
@@ -21,18 +21,10 @@ private:
 };
 
 extern "C" {
-	E void* animal_new(int age) {
-		return new Animal(age);
-	}
-	E int animal_get_age(void* obj) {
-		return ((Animal*)obj)->get_age();
-	}
-	E void animal_get_older(void* obj) {
-		((Animal*)obj)->get_older();
-	}
-	E void animal_dispose(void* obj) {
-		delete (Animal*)obj;
-	}
+	E void* animal_new(int age) { return new Animal(age); }
+	E int	animal_get_age(void* obj) { return ((Animal*)obj)->get_age(); }
+	E void	animal_get_older(void* obj) { ((Animal*)obj)->get_older(); }
+	E void	animal_dispose(void* obj) { delete (Animal*)obj; }
 }
 
 // Inspired in Invoking a C++ method (instance)
