399413dafe232b0a9de8dfc7590cc9832ced92b6
AlanAssembly <harveytriana@hotmail.com>
2021-12-28 15:40:05 -0500

    "C"

--- BlazorCrank/CppLibrary/Animal.cpp
+++ BlazorCrank/CppLibrary/Animal.cpp
@@ -21,19 +21,8 @@ private:
 };
 
 extern "C" {
-	E void* animal_new(int age) {
-		return new Animal(age);
-	}
-	E int animal_get_age(void* obj) {
-		return ((Animal*)obj)->get_age();
-	}
-	E void animal_get_older(void* obj) {
-		((Animal*)obj)->get_older();
-	}
-	E void animal_dispose(void* obj) {
-		delete (Animal*)obj;
-	}
+	E void* animal_new(int age) { return new Animal(age); }
+	E int	animal_get_age(void* obj) { return ((Animal*)obj)->get_age(); }
+	E void	animal_get_older(void* obj) { ((Animal*)obj)->get_older(); }
+	E void	animal_dispose(void* obj) { delete (Animal*)obj; }
 }
-
-// Inspired in Invoking a C++ method (instance)
-// https://www.youtube.com/watch?v=lVWQkpcVEWQ&t=173
